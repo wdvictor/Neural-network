015de562001892ce2444c8a70c39e6cbae2a75c9
SysVic <wd.victorhugo@gmail.com>
2018-11-27 00:37:34 -0200

    fixed some bugs to avoid cascade effect

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 
 
-#define final_vetor_size 2
+#define final_vetor_size 5
 
 struct Neuron
 {
@@ -23,13 +23,13 @@ typedef struct Neuron N;
 
 /*functions*/
 double activate_function(long double n);
-N * create_empty_levels();
-N * initialize_hide_level(N * node , int qtde_of_neurons, long double input[]);
-N * data_level(long double input[]);
+N * create_empty_layers();
+N * initialize_layer(N * node , int qtde_of_neurons, int prev_layer_size);
+N * data_layer(long double input[]);
 N * create_new_neuron(long double input);
-N * create_hide_level(int level_size);
-void local_inducted_field(N * data_level , N * hide_level, int hide_level_size);
-
+N * create_hidden_layer(int layer_size);
+void local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size, int present_layer_size);
+N * output_sinal(N * hidden_layer, N * output_layer);
 
 
 int main(int argc, char const *argv[])
@@ -37,24 +37,36 @@ int main(int argc, char const *argv[])
 	time_t t;
 	srand((unsigned) time(&t));
 	//printf("1\n");
-	N * input_data = create_empty_levels(input_data); /*creating the first element to pass to reference to other`s functinos*/
+	N * input_data = create_empty_layers(); /*creating the first element to pass to reference to other`s functinos*/
 	//printf("2\n");
-	N * hide_process = create_empty_levels(hide_process);
+	N * hidden_process = create_empty_layers();
 	//printf("3\n");
-	long double test_vector[] = {2,1};
+	long double test_vector[] = {2,1, 10, 0 ,-1};
 	//printf("4\n");
-	input_data = data_level(test_vector);
+	input_data = data_layer(test_vector);
 	//printf("5\n");
-	hide_process = create_hide_level(2);
+	hidden_process = create_hidden_layer(5);
 	//printf("6\n");
-	hide_process = initialize_hide_level(hide_process , 2 , test_vector);
+	hidden_process = initialize_layer(hidden_process , 5 , 5);
 
-	local_inducted_field(input_data, hide_process, 2);
+  	N * output_layer = create_empty_layers();
+  	output_layer = initialize_layer(hidden_process, 1 , 5);
 
-	N * temp = hide_process;
+
+
+	local_inducted_field(input_data, hidden_process, 5,  5);
+	local_inducted_field(hidden_process , output_layer , 5 , 1);
+
+	printf("S->%Lf\n",output_layer->output);
+
+	N * temp = hidden_process;
 	while(temp != NULL) 
 	{
+		printf("****************************\n");
 		printf("Nv = %Lf\n", temp->output);
+		printf("B = %d\n", temp->baias);
+		printf("1v = %Lf\n", *(temp->weight + 0));
+		printf("****************************\n");
 		temp =  temp->next;
 	}
 
@@ -70,31 +82,34 @@ double activate_function(long double n)
 
 }
 
-N * create_empty_levels()
+N * create_empty_layers()
 {
 	return NULL;
 
 }
 
-N * initialize_hide_level(N * node , int level_size, long double input[])
+N * initialize_layer(N * node , int layer_size, int prev_layer_size)
 {
+	/*The prev_layer_size variable is use to take the quantity
+	of neuron in the previously layer
+	*/
 	N * temp = node;
 	/*now i gonna distribute the vector of weight and baias for the neurons */
-	for(size_t i = 0 ; i < level_size ; i++)
+	for(size_t i = 0 ; i < layer_size ; i++)
 	{ 
 		//printf("lv(1)%zu\n", i);
-		long double  * weight = (long double *) malloc(final_vetor_size * sizeof(long double));
-		for(size_t j = 0 ; j < final_vetor_size ; j++)
+		long double  * weight = (long double *) malloc(prev_layer_size * sizeof(long double));
+		for(size_t j = 0 ; j < prev_layer_size ; j++)
 		{
 			/*Inside this for I gonna distribute the weight`s for all neuron. But to see
 			more clearly what i do, let`s talk about more the theory.
-			All neuron in the region on the input(the data level) pass his data to the hide
-			level multiplying by a weight. This weights is in the graphical representetion of
+			All neuron in the region on the input(the data layer) pass his data to the hidden
+			layer multiplying by a weight. This weights is in the graphical representetion of
 			neural networks is the edges(rows). To implement this is C, i make the neuron of the 
-			hide level have an array with values that represent the "edges" of the previously
-			level. When the data pass to data level to the hide level, it`s multiplying by this
+			hidden layer have an array with values that represent the "edges" of the previously
+			layer. When the data pass to data layer to the hidden layer, it`s multiplying by this
 			weights in the vector in current position of the neuron. For example: if the neuron 2
-			of the data level is passing his value to hide_level, is going be multiplicated by the weight
+			of the data layer is passing his value to hidden_layer, is going be multiplicated by the weight
 			in position 2 in the vector( 1 if the vector is zero indexed)*/
 			//printf("lv(2)%zu\n", j);
 			if(temp == NULL)
@@ -105,32 +120,23 @@ N * initialize_hide_level(N * node , int level_size, long double input[])
 			else
 			{
 				//printf("lv(3)\n");
-				*(weight + j) = (rand() % (16000 - (-16000) + 1)) + -(16000); 
+				*(weight + j) = rand() % 5 ;//(rand() % (16000 - (-16000) + 1)) + -(16000); 
 				/*A random value between -16000 and 16000*/
 				//printf("lv(4)%Lf\n", *(weight + j));
 			}
 		}
 		//printf("lv(5)\n");
 		temp->weight = weight;
-
-		for(size_t j = 0 ; j < final_vetor_size ; j++)
-		{
-			//printf("Lv(5.1)W-->%Lf\n",*(temp->weight + j));
-
-		}
-		
-
-		temp->baias = (rand() % (16000 - (-16000) + 1)) + -(16000); 
+		temp->baias = rand() % 2 ;//(rand() % (16000 - (-16000) + 1)) + -(16000); 
 		//printf("lv(6)\n");
 		temp = temp->next;
-
 		//printf("lv(Final)\n");
 	}
 	temp = node;
 	return temp;
 }
 
-N * data_level(long double input[])
+N * data_layer(long double input[])
 {
 	N * node = NULL;
 	N * temp = node;
@@ -172,6 +178,7 @@ N * create_new_neuron(long double input)
 		return NULL;
 	}
 	new_neuron->output = input;
+	new_neuron->weight = NULL;
 	new_neuron->baias = 0;
 	new_neuron->next = NULL;
 
@@ -180,61 +187,63 @@ N * create_new_neuron(long double input)
 	return new_neuron;
 }
 
-N * create_hide_level(int level_size)
+N * create_hidden_layer(int layer_size)
 {
-	/*this function create the level with the input in the beginning of the program*/
-	N * level = NULL;
-	for(size_t i = 0 ; i < level_size; i ++)
+	/*this function create the layer with the input in the beginning of the program*/
+	N * layer = NULL;
+	for(size_t i = 0 ; i < layer_size; i ++)
 	{
-		if(level == NULL)
+		if(layer == NULL)
 		{
-			level = create_new_neuron(0); 
+			layer = create_new_neuron(0); 
 			/*it`s just a initialization, what i really need is the list.*/
 		}
-		else if(level->next == NULL)
+		else if(layer->next == NULL)
 		{
-			level->next = create_new_neuron(0);  
+			layer->next = create_new_neuron(0);  
 		}
 		else{
-			N * temp = level;
+			N * temp = layer;
 			while(temp->next != NULL) temp=temp->next;
 			temp->next = create_new_neuron(0);
 		}
 	}
 
-	return level;
+	return layer;
 }
 
-void local_inducted_field(N * data_level , N * hide_level, int hide_level_size)
+void local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size, int present_layer_size)
 {
-	printf("LIF(1)\n");
-	N * data_temp = data_level;
-	if(data_level == NULL) return;
-	printf("LIF(2)\n");
-	N * hide_level_temp = hide_level;
-	if(hide_level == NULL) return ;
+	//printf("LIF(1)\n");
+	N * data_temp = prev_layer;
+	if(prev_layer == NULL) return;
+	//printf("LIF(2)\n");
+	N * present_temp = present_layer;
+	if(present_temp == NULL) return ;
 	long double sum = 0;
-	printf("LIF(3)\n");
-	for(size_t i = 0 ; i < hide_level_size ; i++)
+	//printf("LIF(3)\n");
+	for(size_t i = 0 ; i < present_layer_size ; i++)
 	{
-		printf("----------------------------%zu----------------------------\n",i);
-		for(size_t j = 0 ; j < final_vetor_size ; j++)
+		sum = 0;
+		//printf("----------------------------%zu----------------------------\n",i);
+		for(size_t j = 0 ; j < prev_layer_size ; j++)
 		{
-			printf("--------------------%zu--------------------\n",j);
-			sum += (data_temp->output * *(hide_level_temp->weight + j));
-			printf("LIF(6)\n");
+			//printf("--------------------%zu--------------------\n",j);
+			sum += (data_temp->output * *(present_temp->weight + j));
+			//printf("LIF(6)\n");
 			data_temp = data_temp->next;
 		}
-		printf("LIF(7)\n");
-		sum += hide_level_temp->baias;
-		printf("LIF(8)\n");
-		hide_level_temp->output = activate_function(sum);
-		printf("LIF(9)\n");
-		hide_level_temp = hide_level_temp->next;
-		printf("LIF(10)\n");
-		data_temp = data_level;
-		printf("LIF(Final)\n");
+		//printf("LIF(7)\n");
+		sum += present_temp->baias;
+		//printf("LIF(8)\n");
+		present_temp->output = activate_function(sum);
+		//printf("LIF(9)\n");
+		present_temp = present_temp->next;
+		//printf("LIF(10)\n");
+		data_temp = prev_layer;
+		//printf("LIF(Final)\n");
 	}
 
 	
 }
+
