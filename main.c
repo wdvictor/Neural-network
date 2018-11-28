0c7265a33b4e3ce29ee7ed7cb4bd70f3e3a4e677
SysVic <wd.victorhugo@gmail.com>
2018-11-27 23:44:34 -0200

    the first part of the backpropagation is done

--- main.c
+++ main.c
@@ -12,7 +12,8 @@
 struct Neuron
 {
 
-	long double output;
+	long double input; /*the value that enter in the neuron*/
+	long double output; /*the value that out of the neuron*/
 	int baias;
 	long double * weight;
 	struct Neuron * next;
@@ -29,41 +30,78 @@ N * data_layer(long double input[]);
 N * create_new_neuron(long double input);
 N * create_hidden_layer(int layer_size);
 N * local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size, int present_layer_size);
-
+double error(long double current_error , int expected_output);
+N * output_layer_backpropagation(N * hidden_layer, N * output_layer, int prev_layer_size, double global_error);
+double gradient_output(double global_error , double phi);
+double phi_line(double n);
 
 
 int main(int argc, char const *argv[])
 {
 	time_t t;
 	srand((unsigned) time(&t));
-	N * input_data = create_empty_layers(); /*creating the first element to pass to reference to other`s functinos*/
-	N * hidden_process = create_empty_layers();
+	double global_error, G_output; /*G_output is a variable that holds the gradien value of the output neuron
+									is used in backpropagation of the hidden_layer */
 	long double test_vector[] = {2,1, 10, 0 ,-1};
-	input_data = data_layer(test_vector);
-	hidden_process = create_hidden_layer(5);
-	hidden_process = initialize_layer(hidden_process , 5 , 5);
 
+	N * input_data = create_empty_layers(); /*creating the first element to pass to reference to other`s functinos*/
+	N * hidden_layer = create_empty_layers();
+	
+
+	/*the input_data is the first layer of our neural network, is 
+	the layer of the datas*/
+	input_data = data_layer(test_vector);
+
+	/*the hidden_layer os the hidden layer of our neural network*/
+
+	hidden_layer = create_hidden_layer(5);
+
+	/*the initialize_layer function works to distribute the weights and bias
+	to neurons */
+	hidden_layer = initialize_layer(hidden_layer , 5 , 5);
+
+	/*the output_layer is the neuron of the output */
   	N * output_layer = create_new_neuron(0);
   	output_layer = initialize_layer(output_layer, 1 , 5);
 
 
+  	/* the local_inducted_field function works to do the calculs of the neural
+		network
+  	 */
+	hidden_layer = local_inducted_field(input_data, hidden_layer, 5,  5);
+	output_layer = local_inducted_field(hidden_layer , output_layer , 5 , 1);
+	global_error = error(output_layer->output , 1);
 
-	hidden_process =  local_inducted_field(input_data, hidden_process, 5,  5);
-	output_layer = local_inducted_field(hidden_process , output_layer , 5 , 1);
-
-	printf("S->%Lf\n",output_layer->output);
-	printf("S->%d\n",output_layer->baias);
-
-	N * temp = hidden_process;
-	while(temp != NULL) 
+	printf("Error:%lf\n", global_error);
+	for(size_t i = 0 ; i < 2 ; i++)
 	{
-		printf("****************************\n");
-		printf("Nv = %Lf\n", temp->output);
-		printf("B = %d\n", temp->baias);
-		printf("1v = %Lf\n", *(temp->weight + 0));
-		printf("****************************\n");
-		temp =  temp->next;
+		printf("W(1)|->%Lf\n", *(output_layer->weight + i));
 	}
+
+
+
+	output_layer = output_layer_backpropagation(hidden_layer, output_layer,  2,  global_error);
+
+	printf("****************************\n");
+	for(size_t i = 0 ; i < 2 ; i++)
+	{
+		printf("W(3)|->%Lf\n", *(output_layer->weight + i));
+	}
+
+	 
+	
+
+	// N * temp = hidden_layer;
+	// while(temp != NULL) 
+	// {
+	// 	printf("****************************\n");
+	// 	printf("V() = %Lf\n", temp->input);
+	// 	printf("PHI = %Lf\n", temp->output);
+	// 	printf("B = %d\n", temp->baias);
+	// 	printf("1v = %Lf\n", *(temp->weight + 0));
+	// 	printf("****************************\n");
+	// 	temp =  temp->next;
+	// }
 
 
 
@@ -167,6 +205,7 @@ N * create_new_neuron(long double input)
 		printf("Failed to alloc node.\n");
 		return NULL;
 	}
+	new_neuron->input = input;
 	new_neuron->output = input;
 	new_neuron->weight = NULL;
 	new_neuron->baias = 0;
@@ -204,9 +243,12 @@ N * local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size
 {
 	N * data_temp = prev_layer;
 	if(prev_layer == NULL) return NULL;
+
 	N * present_temp = present_layer;
 	if(present_temp == NULL) return NULL ;
+
 	long double sum = 0;
+
 	for(size_t i = 0 ; i < present_layer_size ; i++)
 	{
 		sum = 0;
@@ -216,6 +258,7 @@ N * local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size
 			data_temp = data_temp->next;
 		}
 		sum += present_temp->baias;
+		present_temp->input = sum;  
 		present_temp->output = activate_function(sum);
 		present_temp = present_temp->next;
 		data_temp = prev_layer;
@@ -225,3 +268,59 @@ N * local_inducted_field(N * prev_layer , N * present_layer, int prev_layer_size
 	return present_layer;
 }
 
+double error(long double current_error , int expected_output)
+{
+	return current_error - expected_output;
+}
+
+double phi_line(double n)
+{
+
+	double up = exp((-1) * n);
+	double down = (1 + exp((-1) * n));
+	double phi = up/pow(down, 2);
+
+	return phi;
+}
+
+double gradient_output(double global_error , double phi)
+{
+	return global_error * phi;
+}
+
+N * output_layer_backpropagation(N * hidden_layer, N * output_layer, int prev_layer_size, double global_error)
+{
+	double learn_rate = 0.2;
+	N * temp = hidden_layer;
+	double delta = 0;
+	for(size_t i = 0; i < prev_layer_size ; i++)
+	{
+		printf("*********************************\n");
+		printf("learn_rate = %lf\n", learn_rate);
+		printf("global_error = %lf\n", global_error);
+		printf("gradient_output = %lf\n", gradient_output(global_error , phi_line(output_layer->output)));
+		printf("phi_line = %lf\n", phi_line(output_layer->output));
+		printf("temp->output = %Lf\n", temp->output);
+		printf("temp->input= %Lf\n", temp->input);
+		printf("*********************************\n");
+		delta = (double) (learn_rate * global_error *  gradient_output(global_error , phi_line(output_layer->output)) * temp->output);
+		printf("Delta = %lf\n", delta);
+		*(output_layer->weight + i) += delta;
+		temp = temp->next;
+	}
+
+	return output_layer;
+}
+
+
+N * hidden_layer_backpropagation(N * hidden_layer, N * output_layer, int prev_layer_size, double global_error)
+{
+	double learn_rate = 0.2;
+	N * hidden_temp = hidden_layer;
+
+	double delta;
+
+
+	
+	
+}
